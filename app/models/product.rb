5bcce476fe4c14f68942e6dfe3f789db110340a0
Gustavo Robles <gustavo.robles@magmalabs.io>
2017-08-14 22:11:29 -0500

    Update product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,7 @@
 class Product
   include Elasticsearch::Persistence::Model
   attribute :name, String
-  attribute :reviews_url, String
+  attribute :rating, Object
+  attribute :total_reviews, Integer
+  attribute :reviews, Array
 end
