f3fab154519181997bd63775c11be29fd1a3efb2
Gustavo Robles <gustavo.robles@magmalabs.io>
2017-08-14 22:08:47 -0500

    Add vcr cassettes

--- spec/workers/product_updater_spec.rb
+++ spec/workers/product_updater_spec.rb
@@ -2,18 +2,19 @@ require 'rails_helper'
 
 describe ProductUpdater do
   describe '#perform' do
+    let(:product) { Product.create(id: 'B002QYW8LW') }
     before do
-      product = Product.new(id: 'B002QYW8LW')
       expect(Product).to receive(:all).and_return [product]
     end
 
     it 'updates product information from amazon' do
       VCR.use_cassette('amazon_lookup') do
-        job = ProductUpdater.new
-        job.perform
-        product = Product.find('B002QYW8LW')
-        expect(product.name).to eq 'Baby Banana Infant Training Toothbrush and Teether, Yellow'
-        expect(product.reviews_url).to eq 'https://www.amazon.com/reviews/iframe?akid=AKIAJLF4KCDNSCNVJ25A&amp;alinkCode=xm2&amp;asin=B002QYW8LW&amp;atag=jscc0a-20&amp;exp=2017-08-15T05%3A48%3A22Z&amp;v=2&amp;sig=39uP84vSB7dY1zkcp7mwBTQKKxpxE7oEPxNoIAwYGPk%253D'
+        VCR.use_cassette('amazon_reviews') do
+          job = ProductUpdater.new
+          expect(product.total_reviews).to be nil
+          job.perform
+          expect(Product.find('B002QYW8LW').total_reviews).to eq 6024
+        end
       end
     end
   end
