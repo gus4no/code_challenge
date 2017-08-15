8645d2049001fbc41fbaf349de78c7eb11c79fdb
Gustavo Robles <gustavo.robles@magmalabs.io>
2017-08-14 22:09:21 -0500

    Add product updater worker

--- app/workers/product_updater.rb
+++ app/workers/product_updater.rb
@@ -3,11 +3,7 @@ class ProductUpdater
 
   def perform
     Product.all.each do |product|
-      response = Amazon::Ecs.item_lookup(product.id, { ResponseGroup: 'Small, Reviews' })
-      item = response.get_element("Item")
-      product.name = item.get_element('ItemAttributes').get('Title')
-      product.reviews_url = item.get_element('CustomerReviews').get('IFrameURL')
-      product.save
+      AmazonProductImporter.import(product.id)
     end
   end
 end
